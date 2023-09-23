b5325a707653b55f754c8ff9f5986fa370a2da71
AlfredoMc95 <alfredomartcam@gmail.com>
2023-09-23 18:23:59 -0500

    Product cuntiti

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
 
 const card = ({ color, greeeting, setCount, countCart }) => {
+  const [product, setProduct] = useState(0);
   const add = () => {
-    setCount(countCart + 1);
+    setProduct(product + 1);
   };
-
   const remove = () => {
-    countCart >= 1 ? setCount(countCart - 1) : setCount((countCart = 0));
+    product >= 1 ? setProduct(product - 1) : setProduct((product = 0));
+  };
+  const addToCart = () => {
+    setCount((countCart = product));
   };
   return (
     <Card
@@ -27,12 +31,27 @@ const card = ({ color, greeeting, setCount, countCart }) => {
         <Typography variant="h2" align="center">
           {greeeting}
         </Typography>
-        <Box sx={{ textAlign: "center" }}>
-          <Button sx={{ m: 2 }} onClick={add} variant="contained">
-            add
-          </Button>
+        <Box
+          sx={{
+            textAlign: "center",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
           <Button onClick={remove} variant="contained">
-            remove
+            -
+          </Button>
+          <Paper sx={{ px: 4, py: 1 }}>{product}</Paper>
+          <Button onClick={add} variant="contained">
+            +
+          </Button>
+          <Button
+            sx={{ mx: 2 }}
+            onClick={addToCart}
+            variant="contained"
+            color="info"
+          >
+            add
           </Button>
         </Box>
       </CardContent>
