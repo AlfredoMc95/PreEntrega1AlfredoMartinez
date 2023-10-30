54c9a86708be5bce69ea6510aaafa4539e5f3e0e
AlfredoMc95 <alfredomartcam@gmail.com>
2023-10-29 20:38:56 -0500

    form falta funcionalidad

--- src/pages/ConfirmBuyPage.jsx
+++ src/pages/ConfirmBuyPage.jsx
@@ -1,9 +1,63 @@
-import { Container } from "@mui/material";
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
 
 const ConfirmBuyPage = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("entro");
+  };
   return (
     <>
-      <Container sx={{ py: 10 }}>Paguina de confirmacion</Container>
+      <Container sx={{ py: 10 }}>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{
+            textAlign: "center",
+            background: "white",
+            borderRadius: 2,
+            p: 2,
+            display: "flex",
+            justifyContent: "center",
+            flexFlow: "column",
+            gap: 2,
+          }}
+        >
+          <Typography variant="h3" component="div">
+            Please validate your info
+          </Typography>
+          <TextField
+            id="name"
+            label="Name"
+            type="text"
+            required
+            variant="filled"
+          />
+          <TextField
+            id="lastName"
+            label="Last name"
+            type="text"
+            required
+            variant="filled"
+          />
+          <TextField
+            id="phone"
+            label="Phone"
+            required
+            type="text"
+            variant="filled"
+          />
+          <TextField
+            id="email"
+            label="Email"
+            required
+            type="email"
+            variant="filled"
+          />
+          <Button type="submit" variant="contained">
+            Confirm
+          </Button>
+        </Box>
+      </Container>
     </>
   );
 };
